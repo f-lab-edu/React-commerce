09a9bbf3e02e20437caf394a23403c427fc52bec
dongyeopCha <ckehdduq95@gmail.com>
2023-04-30 18:57:52 +0900

    feat: productBox 특별할인 타입 컴포넌트 추가

---
+++ src/components/common/atom/ProductBoxPriceType.stories.tsx
@@ -0,0 +1,11 @@
+import type { StoryObj } from '@storybook/react';
+import ProductBoxPriceType from './ProductBoxPriceType';
+
+export default {
+  title: 'productPriceType',
+  component: ProductBoxPriceType,
+};
+
+type Story = StoryObj;
+
+export const Default: Story = {};
---
+++ src/components/common/atom/ProductBoxPriceType.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledProductBoxPriceType = styled.em`
+  font-size: 1.3em;
+  color: #4684e9;
+  font-weight: 700;
+  font-style: normal;
+`;
+function ProductBoxPriceType() {
+  return <StyledProductBoxPriceType>톡딜가&nbsp;&nbsp;</StyledProductBoxPriceType>;
+}
+
+export default ProductBoxPriceType;

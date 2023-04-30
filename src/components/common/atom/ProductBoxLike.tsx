0173eb59b8c643c607af55118d4302e6d523c5f8
dongyeopCha <ckehdduq95@gmail.com>
2023-04-30 21:43:54 +0900

    feat: productBox 좋아요 컴포넌트 추가 카카오 로그인 구현 후 찜 기능 구현 예정

---
+++ src/components/common/atom/ProductBoxLike.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledProductBoxLike = styled.div`
+  color: rgba(24, 32, 55, 0.7);
+  font-size: 24px;
+`;
+function ProductBoxLike() {
+  // 유저 정보 가져와서 라이크 unlike check
+  // 로그인 안되어있으면 로그인 페이지로 이동
+  return <StyledProductBoxLike>♡</StyledProductBoxLike>;
+}
+
+export default ProductBoxLike;

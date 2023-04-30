411607d99179671503fa4652d341156c5a1f7fd5
dongyeopCha <ckehdduq95@gmail.com>
2023-04-30 19:08:02 +0900

    productBox 상품 구매 유저 프로필, 상품 구매자 수 결합 컴포넌트 추가

---
+++ src/components/common/molecule/ProductBoxAboutBoughtUser.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import ProductBoughtUserProfileList from './ProductBoughtUserProfileList';
+import ProductBuyerNumber from '../atom/ProductBuyerNumber';
+
+const StyledProductBoxAboutBoughtUser = styled.span`
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+interface ProductBoxAboutBoughtUserInterface {
+  profiles: string[];
+  groupDiscountUserCount: number;
+}
+function ProductBoxAboutBoughtUser({ profiles, groupDiscountUserCount }: ProductBoxAboutBoughtUserInterface) {
+  return (
+    <StyledProductBoxAboutBoughtUser>
+      <ProductBoughtUserProfileList profileUrlList={profiles} />
+      <ProductBuyerNumber buyer={groupDiscountUserCount} />
+    </StyledProductBoxAboutBoughtUser>
+  );
+}
+
+export default ProductBoxAboutBoughtUser;

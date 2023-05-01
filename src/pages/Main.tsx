b0be20ac4486c7caf314584526ef2ca61a0d740e
dongyeopCha <ckehdduq95@gmail.com>
2023-05-01 15:01:16 +0900

    chore: 패키지 설치 및 린트 룰 수정
    스프레드 프롭스,디폴트 프롭스 등 룰 제거
    ts-config-path 웹팩 플러그인 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '@components/main/organism/Header';
 import Carousel from '@components/main/organism/Carousel';
-import React, { useEffect } from 'react';
 
+const StyledMain = styled.div`
+  display: flex;
+  padding: 80px 0 0 0;
+`;
 export default function Main() {
   return (
-    <>
+    <StyledMain>
+      <Header />
       <Carousel />
-      <br />
-    </>
+    </StyledMain>
   );
 }

1b93407062f97fa534dc85cf3a34fb76ecb9e963
dongyeopCha <ckehdduq95@gmail.com>
2023-05-02 01:31:52 +0900

    feat: 아이콘 컴포넌트, 서브카테고리 컴포넌트 결합

--- src/components/main/organism/Header.tsx
+++ src/components/main/organism/Header.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import SubCategorySet from '@components/main/atom/SubCategorySet';
 import CategoryItemGroup from '../molecule/CategoryItemGroup';
+import HeaderIconGroup from '../molecule/HeaderIconGroup';
+import HeaderLogin from '../atom/HeaderLogin';
 
+const StyledInnerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  height: 80px;
+  width: 1280px;
+  margin: 0 auto;
+`;
 const StyledHeader = styled.div`
   border-bottom: 1px solid #e5e5e5;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  background-color: #fff;
   position: fixed;
+  z-index: 1000;
   left: 0;
   right: 0;
   top: 0;
-  background-color: #fff;
-  height: 80px;
 `;
 function Header() {
   return (
     <StyledHeader>
-      <div>쇼핑하기 아이콘</div>
-      <CategoryItemGroup />
+      <StyledInnerHeader>
+        <span>아이콘</span>
+        <CategoryItemGroup />
+        <SubCategorySet />
+        <HeaderIconGroup />
+        <HeaderLogin />
+      </StyledInnerHeader>
     </StyledHeader>
   );
 }

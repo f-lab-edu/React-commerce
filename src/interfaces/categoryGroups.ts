03fabf03755569ce17c9d76f991e0cf4586be335
dongyeopCha <ckehdduq95@gmail.com>
2023-05-02 01:28:21 +0900

    chore: 인터페이스 수정

--- src/interfaces/categoryGroups.ts
+++ src/interfaces/categoryGroups.ts
@@ -1,14 +1,18 @@
-interface category {
+export interface category {
   categoryId: string;
   name: string;
   level: number;
   sortOrder: number;
 }
 
-export default interface categoryGroup {
+export interface categoryGroup {
   groupId: number;
   name: string;
   sortOrder: number;
   icon: string;
   categories: category[];
+}
+
+export interface categoryData {
+  data: categoryGroup[];
 }

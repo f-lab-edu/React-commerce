930865d1ae743e0454af1a3d46e2b0ae32735e3f
dongyeopCha <ckehdduq95@gmail.com>
2023-05-04 01:53:11 +0900

    feat: middleNavabar 컴포넌트 추가

---
+++ src/components/common/organism/MiddleNavbar.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios';
+import styled from 'styled-components';
+
+interface IMiddleNavbar {
+  title: string;
+  path: string;
+}
+
+export const MiddleNavbar = () => {
+  const [Items, setItems] = useState<IMiddleNavbar[]>();
+  useEffect(() => {
+    axios.get('/middleCategory').then((response: AxiosResponse<IMiddleNavbar[]>) => {
+      setItems(response.data);
+    });
+  }, []);
+
+  const ItemList = Items?.map((item: IMiddleNavbar) => {
+    return (
+      <S.LinkWrap>
+        <S.Navlink to={item.path}>{item.title}</S.Navlink>
+      </S.LinkWrap>
+    );
+  });
+  return <S.MiddelNavbarWrap>{ItemList}</S.MiddelNavbarWrap>;
+};
+
+export const MemoizedMiddleNavbar = React.memo(MiddleNavbar);
+
+const S = {
+  MiddelNavbarWrap: styled.ul`
+    display: flex;
+    font-size: 16px;
+    border: 1px solid #e7e8eb;
+    margin-bottom: 40px;
+  `,
+  LinkWrap: styled.li`
+    flex-basis: 0;
+    flex-grow: 1;
+  `,
+  Navlink: styled(NavLink)`
+    text-decoration: none;
+    display: inline-block;
+    padding: 15px 5px;
+    text-align: center;
+    width: 100%;
+    color: black;
+
+    &:hover {
+      color: #437edd;
+    }
+    &.active {
+      background-color: #437edd;
+      color: white;
+    }
+  `,
+};

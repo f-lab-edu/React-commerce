8794105b0e9bc749d5d879d1bec215dab396d1d8
dongyeopCha <ckehdduq95@gmail.com>
2023-05-07 16:36:38 +0900

    style: 패딩top 중복 제거

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from '@components/main/organism/Header';
-import Carousel from '@components/main/organism/Carousel';
 import Layout from '@components/main/template/Layout';
-
-const StyledMain = styled.div`
-  padding: 80px 0 0 0;
-`;
+import Carousel from '@components/main/organism/Carousel';
 
 export default function Main() {
   return (
-    <StyledMain>
-      <Header />
+    <>
       <Carousel />
-      <Layout />
-    </StyledMain>
+      <Layout />;
+    </>
   );
 }

f9db1f920178699bc17b51c3c00fddd5c21fd35f
dongyeopCha <ckehdduq95@gmail.com>
2023-05-15 20:16:30 +0900

    feat: ProductDetailNav 컴포넌트 추가

---
+++ src/components/detail/ProductDescription.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductDescription = () => {
+  return <div>ProductDescription</div>;
+};
+
+export default ProductDescription;
---
+++ src/components/detail/ProductDetailNav.tsx
@@ -0,0 +1,58 @@
+import React, { useContext, useState } from 'react';
+import ProductDetailContext from 'src/context/ProductDetailContext';
+import { ColorSet } from 'src/utils/constant';
+import styled from 'styled-components';
+
+const ProductDetailNav = ({ children }: { children: React.ReactNode }) => {
+  const data = useContext(ProductDetailContext);
+  const [active, setActive] = useState(0);
+  const listItem = [
+    '상세정보',
+    `리뷰 ${data?.review?.reviewCount.toLocaleString('ko-KR')}`,
+    `문의 ${data?.review?.qnaCount.toLocaleString('ko-KR')} `,
+  ];
+  const target = React.Children.toArray(children).filter((child, i) => i === active)[0];
+  return (
+    <>
+      <S.ProductDetailNav>
+        {listItem.map((li, index) =>
+          active === index ? (
+            <S.ActiveProductDetailNavItem>{li}</S.ActiveProductDetailNavItem>
+          ) : (
+            <S.ProductDetailNavItem onClick={() => setActive(index)}>{li}</S.ProductDetailNavItem>
+          )
+        )}
+      </S.ProductDetailNav>
+      <div>{target}</div>
+    </>
+  );
+};
+
+export default ProductDetailNav;
+
+const S = {
+  ProductDetailNav: styled.ul`
+    display: flex;
+  `,
+  ActiveProductDetailNavItem: styled.li`
+    flex: 1;
+    line-height: 60px;
+    text-align: center;
+    font-weight: 600;
+    list-style: none;
+    color: white;
+    background-color: ${ColorSet.textBlack};
+  `,
+  ProductDetailNavItem: styled.li`
+    flex: 1;
+    line-height: 60px;
+    text-align: center;
+    font-weight: 600;
+    list-style: none;
+  `,
+  ProductDescription: styled.div`
+    width: 900px;
+    height: 2200px;
+    overflow: hidden;
+  `,
+};

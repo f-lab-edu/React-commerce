dcbbe54057fc93215c949400e641ae3add9b4cb4
dongyeopCha <ckehdduq95@gmail.com>
2023-05-18 20:14:37 +0900

    style: sideSection sticky적용

--- src/components/detail/SideSection.tsx
+++ src/components/detail/SideSection.tsx
@@ -6,34 +6,42 @@ import OptionSelect from './OptionSelect';
 import OptionDeliveryFee from './OptionDeliveryFee';
 import SelectedProduct from './SelectedProduct';
 import OptionResult from './OptionResult';
+import OptionPurchase from './OptionPurchase';
 
 const SideSection = () => {
   const optionData = useContext(ProductOptionsContext);
 
   if (optionData === null) return null;
   return (
-    <S.Wrap>
-      <S.OptionSelectSection>
-        <S.OptionSelectTitle>옵션선택</S.OptionSelectTitle>
-        {optionData.options?.names.map((e, index) => (
-          <OptionSelect level={index} key={e} />
-        ))}
-        {Object.entries(optionData.products).map(([id, payload]) => (
-          <SelectedProduct key={id} id={id} payload={payload} />
-        ))}
-      </S.OptionSelectSection>
-      <OptionDeliveryFee />
-      <OptionResult products={optionData.products} />
-    </S.Wrap>
+    <S.SideSection>
+      <S.Wrap>
+        <S.OptionSelectSection>
+          <S.OptionSelectTitle>옵션선택</S.OptionSelectTitle>
+          {optionData.options?.names.map((e, index) => (
+            <OptionSelect level={index} key={e} />
+          ))}
+          {Object.entries(optionData.products).map(([id, payload]) => (
+            <SelectedProduct key={id} id={id} payload={payload} />
+          ))}
+        </S.OptionSelectSection>
+        <OptionDeliveryFee />
+        <OptionResult products={optionData.products} />
+        <OptionPurchase products={optionData.products} />
+      </S.Wrap>
+    </S.SideSection>
   );
 };
 
 export default SideSection;
 
 const S = {
-  Wrap: styled.div`
-    flex: 1;
+  SideSection: styled.div`
     padding: 30px 0 30px 30px;
+    flex: 1;
+  `,
+  Wrap: styled.div`
+    position: sticky;
+    top: 100px;
     min-height: 350px;
   `,
   OptionSelectSection: styled.div`

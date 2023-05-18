152413a61a2a56e5d71cb5c09909f83df8d9ebc7
dongyeopCha <ckehdduq95@gmail.com>
2023-05-18 14:16:03 +0900

    style: 배경색상 상수 추가

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,11 +4,13 @@ const ColorSet = {
   textGray: '#999',
   textOrange: '#fa8300',
   textBlack: '#333',
+  textRed: '#ff5959',
   borderGray: '#D8DAE1',
   borderBlue: '#bad3f5',
   borderOrange: '#f5d6ba',
   backgroundYellow: '#ffeb00',
   backgroundBlack: '#1e1e1e',
+  backgroundGray: '#f7f9fa',
 } as const;
 
 export { RECENT_KEYWORD, ColorSet };

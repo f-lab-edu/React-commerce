141334404b1df7ceb5e3aeef871cce610a04efad
dongyeopCha <ckehdduq95@gmail.com>
2023-05-24 09:37:39 +0900

    fix: strictMode Off

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,10 @@ worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <>
     <GlobalStyle />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
